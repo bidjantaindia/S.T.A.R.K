906c15bdbd2336285dd75fe549f5de9ca3f3a055
ravisingh00007 <116331588+ravisingh00007@users.noreply.github.com>
2022-11-09 16:36:42 +0530

    commitr

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-    // change is made.
+    // change is ravi.
 };
+
+
 
